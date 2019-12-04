98ccf9d3337d1def2163cd86b6c5a2f46d22cba4
mabaixiao <mabaixiao@foxmail.com>
2019-12-04 16:27:46 +0800

    首次提交

---
+++ app/src/main/java/com/mabaixiao/dashboard/Dashboard.java
@@ -0,0 +1,75 @@
+package com.mabaixiao.dashboard;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Dashboard extends View {
+
+    private int mHeight, mWidth;
+
+    public Dashboard(Context context) {
+        super(context);
+    }
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Dashboard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //获取宽高
+        mHeight = getMeasuredHeight();
+        mWidth = getMeasuredWidth();
+        //画外圆
+        Paint paintCircle = new Paint();
+        paintCircle.setStyle(Paint.Style.STROKE);//空心
+        paintCircle.setAntiAlias(true);//锯齿效果
+        paintCircle.setStrokeWidth(5);//边框宽度
+        canvas.drawCircle(mWidth / 2, mHeight / 2, mWidth / 2, paintCircle);
+        //画刻度
+        Paint paintDegree = new Paint();
+        paintCircle.setStrokeWidth(3);
+        for (int i = 0; i < 24; i++) {
+            //区分整点还是非整点
+            if (i == 0 || i == 6 || i == 12 || i == 18) {
+                paintDegree.setStrokeWidth(5);
+                paintDegree.setTextSize(30);
+                canvas.drawLine(mWidth / 2, mHeight / 2 - mWidth / 2,
+                        mWidth / 2, mHeight / 2 - mWidth / 2 + 60, paintDegree);
+                String degree = String.valueOf(i);
+                canvas.drawText(degree, mWidth / 2 - paintDegree.measureText(degree) / 2,
+                        mHeight / 2 - mWidth / 2 + 90, paintDegree);
+            } else {
+                paintDegree.setStrokeWidth(3);
+                paintDegree.setTextSize(15);
+                canvas.drawLine(mWidth / 2, mHeight / 2 - mWidth / 2,
+                        mWidth / 2, mHeight / 2 - mWidth / 2 + 30, paintDegree);
+            }
+            //通过旋转画布简化坐标运算
+            canvas.rotate(15, mWidth / 2, mHeight / 2);
+        }
+        //画圆心
+        Paint paintPointer = new Paint();
+        paintPointer.setStrokeWidth(30);
+        canvas.drawPoint(mWidth / 2, mHeight / 2, paintPointer);
+        //画指针
+        Paint paintHour = new Paint();
+        paintHour.setStrokeWidth(20);
+        Paint paintMinute = new Paint();
+        paintMinute.setStrokeWidth(10);
+        canvas.save();
+        canvas.translate(mWidth / 2, mHeight / 2);
+        canvas.drawLine(0, 0, 100, 100, paintHour);
+        canvas.drawLine(0, 0, 100, 200, paintMinute);
+        canvas.restore();
+    }
+}
